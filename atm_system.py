7a8baf09f6d48c4dea3abf5a9b11a9905f945eb2
Somto <128147364+SectorInc@users.noreply.github.com>
2026-06-23 23:34:38 +0100

    Update atm_system.py

--- atm_system.py
+++ atm_system.py
@@ -1,11 +1,8 @@
 
 # ATM Banking System
-# Author: [Your Name]
-# Description: An ATM simulation that supports
-#              PIN login, withdrawal, balance
-#              checks, and account transfers.
 
-# --- User Database (sim) ---
+
+#  User Database 
 users = {
     "user1": {"name": "John Doe",  "pin": "1234", "balance": 2000.00},
     "user2": {"name": "Jane Smith","pin": "5678", "balance": 1000.00},
